88e3d833dc43cc152eea7a5552cf4e4c3356a19d
ethar <mahmoudethar501@gmail.com>
2024-09-09 15:19:11 +0300

    add custom list view item to home

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
 const kprimaryColor =  Color(0xff100B20) ;
+
 const kTransition = Duration(milliseconds: 250);
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,9 @@
 import 'package:bookly_app/core/utlis/assests.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,7 +11,9 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [customAppBar()],
+      children: [customAppBar(),
+      customListViewItem()],
+     
     );
   }
 }
