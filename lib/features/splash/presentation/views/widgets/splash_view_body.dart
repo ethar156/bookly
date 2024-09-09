2ffe57a617f29e2748c61514bc4b05ad4ae61d65
ethar <mahmoudethar501@gmail.com>
2024-09-09 11:33:23 +0300

    refactor init state and navigate to home

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,12 +1,42 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utlis/assests.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animatedController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    
+    navigateToHome();
+  }
+
+  
+  @override
+  void dispose() {
+    
+    super.dispose();
+    animatedController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -14,11 +44,28 @@ class SplashViewBody extends StatelessWidget {
         const SizedBox(
           height: 4,
         ),
-        const Text(
-          'Read Free Books',
-          textAlign:TextAlign.center)
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
-
     );
   }
+
+  void initSlidingAnimation() {
+     animatedController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 20), end: Offset.zero)
+            .animate(animatedController);
+    animatedController.forward();
+  }
+
+
+  void navigateToHome() {
+     Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration: kTransition);
+    });
+  }
+
 }
+
